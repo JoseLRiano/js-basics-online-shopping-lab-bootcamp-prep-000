cf429151228fe34aa74c9a10da23784226b01e4e
Jose Riano <joselriano10@gmail.com>
2017-09-27 17:38:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   }
   const arrNameAndPrice =[];//store the objects in cart to later print them
   for(let i =0; i <cart.length;i++){//divide the information
-    var iName = objects.keys(cart[i]);
+    var iName = Object.keys(cart[i]);
     var productName = iName[0];
     var iPrice = cart[i][iName];
     arrNameAndPrice.push(`${iName} at $${iPrice}`);
