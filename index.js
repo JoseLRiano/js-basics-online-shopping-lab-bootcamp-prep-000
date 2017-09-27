11c9f395979863396a71781f77ddda9784fe5c02
Jose Riano <joselriano10@gmail.com>
2017-09-27 17:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
     arrNameAndPrice.push(`${iName} at ${iPrice}`);
   }
   if(arrNameAndPrice.lenght === 1){
-    return console.log(`In your cart, you have ${arrNameAndPrice.join()}.`);
+    return console.log(`In your cart, you have ${arrNameAndPrice[0]}.`);
   } else if (arrNameAndPrice.length === 2) {
     return console.log(`In your cart, you have ${arrNameAndPrice[0]} and ${arrNameAndPrice.slice(-1)}.`);
   } else {
